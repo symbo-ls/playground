d5d3156ea0dbd4229e5e4682dd800b46534e5853
Nika Tomadze <nika.tomadze@gmail.com>
2021-09-10 04:27:20 +0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ import { style } from './canvas/style'
 import preview from './preview'
 import code from './code'
 
+import * as Symbols from '@rackai/symbols'
+window.Symbols = Symbols
+
 export default {
   style,
   preview,
